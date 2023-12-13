6d90b81244d329a132493b4b4460ab7a6275ada7
VU Manh Tu <glmanhtu@gmail.com>
2023-12-13 21:02:14 +0100

    Add some useful distance loss utilities

--- ml_engine/criterion/losses.py
+++ ml_engine/criterion/losses.py
@@ -1,6 +1,27 @@
 import torch
 
 
+class DistanceLoss(torch.nn.Module):
+    def __init__(self, loss_fn, distance_fn):
+        super().__init__()
+        self.criterion = loss_fn
+        self.distance_fn = distance_fn
+
+    def forward(self, predict, actual):
+        return self.criterion(predict, actual)
+
+    def distance(self, predict, actual):
+        return self.distance_fn(predict, actual)
+
+
+class BatchDotProduct(torch.nn.Module):
+    # See https://github.com/pytorch/pytorch/issues/18027
+
+    def forward(self, predict, actual):
+        B, S = predict.shape
+        return torch.bmm(predict.view(B, 1, S), actual.view(B, S, 1)).reshape(-1)
+
+
 class LossCombination(torch.nn.Module):
     def __init__(self, criterions):
         super().__init__()
